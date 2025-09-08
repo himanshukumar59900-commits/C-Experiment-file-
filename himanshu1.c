14391650668b73a034f68db3437ec036e55ae1f4
himanshukumar59900-commits <himanshukumar59900@gmail.com>
2025-09-08 15:33:02 +0530

    Add files via upload

---
+++ himanshu1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char name [50];
+    int age;
+    printf("enter your name")
+    scanf("%s"&name);
+
+    print f (nter your age );
+    scanf ("%d",&age);
+
+    printf ("\n hello %s! you are %d years old.\n",name,age);
+    return 0;
+}
